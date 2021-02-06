7bd92a6e493e9bdfcd0b17c592210c05b5017a88
stanislavdimitrenco <stanislav.dimitrenco@gmail.com>
2021-02-06 23:14:19 +0200

    add handler

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,14 +1,45 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sqs/goreturns/returns"
+)
 
 //Handler struct
 type Handler struct {
-
 }
 
 //InitRouts - initializing routs
 func (h *Handler) InitRouts() *gin.Engine {
 	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("sing-up")
+		auth.POST("sing-in")
+	}
+
+	api := router.Group("/api")
+	{
+		list := api.Group("/list")
+		{
+			list.POST("/")
+			list.GET("/")
+			list.GET("/:id")
+			list.PUT("/:id")
+			list.DELETE("/:id")
+
+			items := list.Group(":id/items")
+			{
+				items.POST("/")
+				items.GET("/")
+				items.GET("/:item_id")
+				items.PUT("/:item_id")
+				items.DELETE("/:item_id")
+			}
+		}
+	}
+
+	return router
 
 }
